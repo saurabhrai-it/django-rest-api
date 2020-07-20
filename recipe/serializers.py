0d3b3fe63068ab2a779f4c8637541c9de93d5325
Saurabh Rai <saurabh.rai@nagarro.com>
2020-07-20 12:36:08 +0530

    Added recipe feature

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -1,4 +1,4 @@
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from rest_framework import serializers
 
 
@@ -18,3 +18,29 @@ class IngredientSerializer(serializers.ModelSerializer):
         model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
+
+
+# https://www.django-rest-framework.org/api-guide/relations/#primarykeyrelatedfield
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'title', 'ingredients', 'tags', 'time_minutes', 'price',
+            'link',
+        )
+        read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
